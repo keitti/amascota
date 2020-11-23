7d9cadddd485116a473627c757034090d61ef243
Ana Maria Vallejo <65957832+anamaria0611@users.noreply.github.com>
2020-11-23 12:54:00 -0700

    front corregido

--- src/views/home/homeContainer.jsx
+++ src/views/home/homeContainer.jsx
@@ -22,7 +22,7 @@ class HomeContainer extends Component {
   render() {
     return (
       <div className="home-container" >
-        <h1>¡Calidad de vida para tu mascota!</h1>
+        <h1>¡Calidad de vidad para tu mascota!</h1>
         <div
           id="carouselExampleControls"
           class="carousel slide"
--- src/views/tips/tipsContainer.jsx
+++ src/views/tips/tipsContainer.jsx
@@ -6,7 +6,6 @@ class TipsContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     
       tips: []
     };
   }
